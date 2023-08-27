c331389c235ef1ec0ad8ee59a132bdf2df3f297b
hiba <griouanihiba@gmail.com>
2023-08-27 22:55:56 +0100

    change1

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,29 @@
-const Layout = () => {
-  return <div>Layout</div>;
-};
+//Importing fonts
+import { Sora } from '@next/font/google';
 
+//Importing components
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLeftImg from '../components/TopLeftImg';
+import Home from '../pages/index';
+
+// Font settings
+const sora = ({
+  Subsets: ['latin'], 
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+// Layout component
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+     <Home/>
+      {/* {children} */}
+    </div>
+  );
+}
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
+//next image
+import Image from "next/image";
+
+//componenet
+import Avatar from '../components/Avatar'
+import ProjectsBtn from '../components/ProjectsBtn'
+import ParticlesContainer from '../components/ParticlesContainer'
+
+//framer motion
+import {motion} from 'framer-motion'
+//variants
+import {fadeIn} from '../variants'
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return(
+   <div  className="bg-primary/60 h-full">
+  {/*text*/}
+  <div  className="w-full h-full bg-gardient-to-r from-primary/10 via-black/30 to-black/10">
+    <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto" >
+{/*title*/}
+<h1 className="h1">Transforming ideas <br/> Info {' '}
+    <span className="text-accent"> Digital Reality</span>
+     </h1>
+       {/*Subtite*/}
+  <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"> bla 2 bla bla bla </p>
+     {/*btn*/}
+     <div className="flex justify-center xl: relative">
+     <ProjectsBtn/>
+    </div>
+    </div>
+  </div>
+  {/*image*/}
+  <div>image</div>
+  </div>)
 };
 
 export default Home;
